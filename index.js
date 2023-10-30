36ee5d82fbd5a520f119d391ee42b04a41b6db8e
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-10-30 15:21:41 -0700

    assigmnent complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+const multiply = num1 * num2;
+
+const random = Math.round(Math.random () +1)
+
+const num3=12;
+const num4=3;
+const mod = num3/num4;
+
+const max = Number.parseInt('20', 10);
